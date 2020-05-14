7c96e93437e5d8f77a0329f992282feb4755056c
surya3217 <2015kuec2015@iiitkota.ac.in>
2020-05-14 23:47:52 +0530

    Mongodb database hands on

---
+++ Database Handling/MongoDB_Database.py
@@ -0,0 +1,69 @@
+"""
+pip install or conda install the libraries
+Create Mongo Atlas Account and Verify it via the email
+https://cloud.mongodb.com
+
+Create a cluster
+Create Database
+Give Database Access
+Give Network Access
+Create Collection 
+Then Connect to get the python 3.4 sample code
+
+# AWS N. Virginia Free Tier - MO Sandbox 
+# Database Access - database access as Admin for new user with Read and Write Access
+# Autopasssword generation should be used
+# Security/Network Access - IP Whitelisting 0.0.0.0/0 ( Access from anywhere)
+"""
+
+'''
+Insert the data to Mongodb Cloud
+'''
+
+import pymongo
+import pandas as pd
+
+cluster = pymongo.MongoClient("mongodb+srv://surajkumar9427:surya123@cluster0-xk6lv.mongodb.net/test?retryWrites=true&w=majority")
+#client = pymongo.MongoClient("mongodb+srv://sylvesterferns:cooler%21%40%23123@cluster0-t6mku.mongodb.net/test?retryWrites=true&w=majority")
+client= cluster['forsk_db']
+collection= client['forsk_coll']
+
+
+def add_employee(idd, first, last, pay):
+    '''Adding the unique employees into the collection'''
+    
+    unique_employee = collection.find_one({"_id":idd})  ## finding the existing idd 
+    if unique_employee:
+        return "Employee already exists"
+    else:
+        collection.insert_one(         ## inserting the data
+                {
+                "_id" : idd,
+                "first" : first,
+                "last" : last,
+                "pay" : pay
+                })
+        return "Employee added successfully"
+
+
+def fetch_all_employee():
+    '''reading the data from the mongodb cloud's collection'''
+    user = collection.find()    ## finding all data from the collection
+    for i in user:
+        print (i)
+
+
+collection.drop()
+
+##Insert data in collection
+add_employee(10,'Sylvester', 'Fernandes', 50000)
+add_employee(2,'Yogendra', 'Singh', 70000)
+add_employee(3,'Rohit', 'Mishra', 30000)
+add_employee(4,'Kunal', 'Vaid', 30000)
+add_employee(12,'Devendra', 'Shekhawat', 30000)
+
+fetch_all_employee()
+
+
+
+
